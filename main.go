b6ea301a92a5b91b820f6bab641bba6f21e4b92f
Nimo <nimo.jser@gmail.com>
2019-06-09 22:09:03 +0800

    check

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package typejson
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/labstack/gommon/log"
 	"github.com/tidwall/gjson"
 	"reflect"
 	"strings"
@@ -42,13 +44,23 @@ func setValue(target interface{}, attr string, value interface{}) {
 }
 type TypesItem struct {
 	Default interface{}
-	check interface{}
+	check func(data TypeItemCheckData) (message string, pass bool)
 }
-
-func Parse(jsonstring string, data interface{}, types map[string]TypesItem){
+type TypeItemCheckData struct {
+	valueNumber float64
+	valueString string
+	valueBool bool
+}
+type ParseInfo struct {
+	Message string
+}
+func Parse(jsonstring string, data interface{}, types map[string]TypesItem)(info ParseInfo, fail bool){
 	json.Unmarshal([]byte(jsonstring), data)
 	for key, schema := range types {
 		attr := key
+		targetResult := gjson.Get(jsonstring, key)
+		targetValue := targetResult.Value()
+		_ = targetValue
 		isEmptyValue := len(gjson.Get(jsonstring, key).Raw) == 0
 		attrLastWord := string([]byte(attr)[len(attr)-1])
 		isNotRequired := false
@@ -60,6 +72,7 @@ func Parse(jsonstring string, data interface{}, types map[string]TypesItem){
 		}
 		shouldSetAttrNil := isEmptyValue && isNotRequired
 		shouldSetDefaultValue := !shouldSetAttrNil && schema.Default != nil
+		shouldCheckValue := !shouldSetAttrNil && schema.check != nil
 		if  shouldSetAttrNil {
 			attr = attr + "Nil"
 			setValue(data, attr, true)
@@ -67,5 +80,29 @@ func Parse(jsonstring string, data interface{}, types map[string]TypesItem){
 		if shouldSetDefaultValue {
 			setValue(data, attr, schema.Default)
 		}
+		if (shouldCheckValue) {
+			var data TypeItemCheckData
+			switch targetResult.Type {
+				case gjson.Number:
+					data.valueNumber = targetResult.Float()
+				case gjson.String:
+					data.valueString = targetResult.String()
+				case gjson.True:
+				case gjson.False:
+					data.valueBool = targetResult.Bool()
+				case gjson.Null:
+					log.Fatal("typejson: " + attr + " is nil!")
+				default:
+					fmt.Print(targetResult.Type)
+					log.Fatal("@TODO: 需要加上 object array")
+			}
+			message, pass := schema.check(data)
+			if !pass {
+				fail = true
+				info.Message = message
+				break
+			}
+		}
 	}
+	return
 }
--- main_test.go
+++ main_test.go
@@ -5,46 +5,33 @@ import (
 	"testing"
 )
 
-func TestValue(t *testing.T) {
-	Convey("Given some integer with a starting value", t, func() {
+func TestParse(t *testing.T) {
+	Convey("default NotRequired",t,  func() {
 		type Author struct {
-		Name  string   `json:"name"`
-		Age   int      `json:"age",omitempty`
-		IsFriendly bool `json:"is_firendly"`
-		Likes []string `json:"likes"`
-		Children struct {
-			Son string `json:"son"`
-			Daughter string `json:"daughter"`
-		} `json:"a"`
-		TestEmptyNumberNil bool
-		TestEmptyNumber int
-		validDemoA int
-		validDemoB int
-	}
-	var nimo Author
-	jsonstring := `
-	{
-		"name":"nimo",
-		"likes": ["js", "go"],
-		"validDemoA": 10,
-		"validDemoB": 20
-	}
-	`
-	Parse(jsonstring, &nimo, map[string]TypesItem{
-		"age" : { Default: 27},
-		"children.son" : { Default: "fifteen"},
-		"children.daughter" : { Default: "wood"},
-		"testEmptyNumber?" : {},
-		"validDemoA": {
-			check: func (value int) (failMessage string, pass bool) {
-				if (value < 18) {
-					return "未成年", false
-				}
-				return "", true
-			},
-		},
-	})
-	Convey("Name: ", func() {
+			Name  string   `json:"name"`
+			Age   int      `json:"age",omitempty`
+			IsFriendly bool `json:"is_firendly"`
+			Likes []string `json:"likes"`
+			Children struct {
+				Son string `json:"son"`
+				Daughter string `json:"daughter"`
+			} `json:"a"`
+			TestEmptyNumberNil bool
+			TestEmptyNumber int
+		}
+		var nimo Author
+		jsonstring := `
+		{
+			"name":"nimo",
+			"likes": ["js", "go"]
+		}
+		`
+		Parse(jsonstring, &nimo, map[string]TypesItem{
+			"age" : { Default: 27},
+			"children.son" : { Default: "fifteen"},
+			"children.daughter" : { Default: "wood"},
+			"testEmptyNumber?" : {},
+		})
 		So(nimo.Name, ShouldEqual, "nimo")
 		So(nimo.Age, ShouldEqual, 27)
 		So(nimo.IsFriendly, ShouldEqual, false)
@@ -55,5 +42,28 @@ func TestValue(t *testing.T) {
 		So(nimo.TestEmptyNumberNil, ShouldEqual, true)
 		So(nimo.TestEmptyNumber, ShouldEqual, 0)
 	})
+	Convey("check",t,  func() {
+		checkTypes := map[string]TypesItem{
+			"age": {
+				check: func (data TypeItemCheckData) (message string, pass bool) {
+					if data.valueNumber < 18 {
+						message = "未成年"; return
+					}
+					pass = true; return
+				},
+			},
+		}
+		type Query struct {
+			age int
+		}
+		var wrongQuery Query
+		queryInfo, queryPassFail := Parse(` { "age": 10 }`, &wrongQuery, checkTypes)
+		So(queryPassFail, ShouldEqual, true)
+		So(queryInfo.Message, ShouldEqual, "未成年")
+
+		var correctQuery Query
+		correctQueryInfo, correctQueryPassFail := Parse(` { "age": 20 }`, &correctQuery, checkTypes)
+		So(correctQueryPassFail, ShouldEqual, false)
+		So(correctQueryInfo.Message, ShouldEqual, "")
 	})
 }
